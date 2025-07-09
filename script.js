c239080dc900d4fa29d4e280b980bec003db09da
diggajbaral <atdiggaz@gmail.com>
2025-07-09 18:12:30 +0545

    time

---
+++ script.js
@@ -0,0 +1,9 @@
+// Set Timeout
+setTimeout(function (){
+    console.log("Hello, after 3 seconds");
+}, 3000)
+
+// Set Interval
+setInterval(function() {
+    console.log("Repeating after every 2 seconds");
+}, 2000)
