22cece7c5613a7ff488babdeb8378ac622a9a701
diggajbaral <bolaribral@gmail.com>
2025-07-23 09:19:54 -0700

    sum of array

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// JavaScript
+// print sum of array. 
+
+function sumArray(arr) {
+
+    sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+
+}
+
+console.log(sumArray([5, 5, 5]));
