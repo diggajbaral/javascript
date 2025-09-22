8dd4c90785c86eef82805b7bc54b0c5beed05fee
diggajbaral <volaribrl@gmail.com>
2025-09-22 04:18:06 -0700

    do while loop

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 
-let index = 0
+let index = 0;
 
 while (index <= 10) {
-    console.log(`Value of Index is ${index}`);
+    // console.log(`Value of Index is ${index}`);
     // index++;
     index = index + 2;
 }
+
+let score = 1;
+
+do {
+    console.log(`Score is ${score}`)
+    score++;
+} while (score <= 10);
