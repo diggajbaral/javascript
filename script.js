4a1ee7de5170db68e5c578563a9d4f91adcf5fc8
diggajbaral <volaribrl@gmail.com>
2025-10-11 01:13:32 +0530

    object loops

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-const user = {
-    name: "jhon",
-    age: 30,
-    contact: {
-        email: "jhon@gmail.com",
-        phone: "213-456-789",
-        address: {
-            city: "new york",
-            zip: "10001"
-        }
+const employee = {
+    name: "Sarah",
+    age: 28,
+    department: "Marketing",
+    skills: {
+        primary: "SEO",
+        secondary: "content writing"
     }
-};
+}
 
-console.log(user.contact.email);
-console.log(user.contact.address.city);
-user.contact.address.country = "USA";
-
-console.log(`${user.name} lives in ${user.contact.address.city}, ${user.contact.address.country}. Contact: ${user.contact.email}`)
+for (let k in employee) {
+    console.log(k, employee[k])
+}
