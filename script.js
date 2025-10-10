6f7b5d62c8dd9fb27403cb03348c01e0da4ff551
diggajbaral <volaribrl@gmail.com>
2025-10-11 00:05:06 +0530

    objets

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
- 
+const student = {
+    name: "Alice",
+    age: 20,
+    grade: "B"
+};
+
+student.grade = "A";
+// console.log(student)
+
+student.city = "london";
+// console.log(student)
+
+console.log(`${student.name} is ${student.age} years old, lives in ${student.city}, and got and ${student.grade} grade.`)
