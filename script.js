e0e167867b310086d4b99ec9e0190017ef33edcf
diggajbaral <volaribrl@gmail.com>
2025-10-11 09:25:34 +0530

    loops in object

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-const employee = {
-    name: "Sarah",
-    age: 28,
-    department: "Marketing",
-    skills: {
-        primary: "SEO",
-        secondary: "content writing"
-    }
-}
+const users = [
+    {name: "alice", age: 22, country: "USA"},
+    {name: "bob", age: 28, country: "UK"},
+    {name: "charlie", age: 25, country: "Canada"}
+]
 
-for (let k in employee) {
-    console.log(k, employee[k])
+for (let user of users) {
+    console.log(`${user.name} is ${user.age} years old and lives in ${user.country}`)
 }
