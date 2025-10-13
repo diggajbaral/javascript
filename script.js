0ddf247f5f93ad6f7d85234272d4efa00e2404d5
diggajbaral <volaribrl@gmail.com>
2025-10-13 08:02:36 +0530

    * pattern

--- script.js
+++ script.js
@@ -1,48 +1,13 @@
-const students = [
-    {
-        name: "Alice",
-        age: 20,
-        grades: { math: 90, english: 85, science: 95 }
-    },
-    {
-        name: "Bob",
-        age: 22,
-        grades: { math: 75, english: 80, science: 70 }
-    },
-    {
-        name: "Charlie",
-        age: 21,
-        grades: { math: 88, english: 92, science: 84 }
-    }
-];
+let dot = 4;
 
-let topStudent = "";
-let highestAverage = 0;
+for (let row = 1; row <= dot; row++) {
 
-for (let student of students) {
+    let star = '';
 
-    let total = 0;
-    let count = 0;
-
-    for (let subject in student.grades) {
-
-        total += student.grades[subject];
-        count++;
-
+    for (let col = 1; col <= dot; col++) {
+        star += '*';
     }
 
-    let average = total / count;
-
-    console.log(`${student.name} - Average : ${average}`);
-
-    if (average > highestAverage) {
-
-
-        highestAverage = average;
-        topStudent = student.name;
-
-    }
+    console.log(star);
 
 }
-
-console.log(`Top student: ${topStudent} with average = ${highestAverage}`);
