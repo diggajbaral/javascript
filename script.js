afaa9ea9c8a66ff4680361aecea2b1fa4de19c26
diggajbaral <volaribrl@gmail.com>
2025-10-16 15:08:24 +0530

    even with while loop

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-let n = 5;
+let i = 0;
 
-for (let i = 1; i <= n; i++) {
-
-    let spaces = ' '.repeat(n - i);
-    let stars = '*'.repeat(2 * i - 1);
-
-    console.log(spaces + stars);
-
+while (i <= 20) {
+    console.log(i);
+    i = i + 2
 }
