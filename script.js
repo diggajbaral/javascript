0708ab801af32f62aec0c7c75bd4ff82767b5b10
diggajbaral <volaribrl@gmail.com>
2025-10-18 11:35:27 +0530

    reverse the array element

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-for (let i = 0; i < 3; i++) {
-    setTimeout(function () {
-        console.log(i);
-    }, 1000);
+const original = [1, 2, 3, 4, 5];
+const reversed = [];
+
+for (let i = original.length - 1; i >= 0; i--) {
+    reversed.push(original[i]);
 }
+
+console.log(reversed);
