199dd5ea096badeca068e3cd107babbf48c7e1ee
diggajbaral <volaribrl@gmail.com>
2025-10-19 16:21:21 +0530

    reverse the string

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const original = [1, 2, 3, 4, 5];
-const reversed = [];
+const user = 'elon musk';
+const result = user.split('').reverse().join('')
 
-for (let i = original.length - 1; i >= 0; i--) {
-    reversed.push(original[i]);
-}
-
-console.log(reversed);
+console.log(result);
