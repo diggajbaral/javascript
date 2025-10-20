d389b773a7ede00fcba0478a03b0cb252d7fde9b
diggajbaral <volaribrl@gmail.com>
2025-10-20 21:17:25 +0530

    create new propertise in object

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-const School = [
+const school = [
     { id: 1, name: "Diggaj Baral", class: 9, fee_pays: 2500 },
     { id: 2, name: "Anjali Sharma", class: 10, fee_pays: 2700 },
     { id: 3, name: "Ramesh Thapa", class: 8, fee_pays: 2300 },
     { id: 4, name: "Sita Rai", class: 9, fee_pays: 2600 },
-    { id: 5, name: "Kiran Gurung", class: 11, fee_pays: 2800 },
-    { id: 6, name: "Krishna Chaudary", fee_pays: 8600 }
+    { id: 5, name: "Kiran Gurung", class: 11, fee_pays: 2800 }
 ];
 
-let groupedStudents = {};
-
-for (const student of School) {
-
-    if (!groupedStudents[student.class]) {
-        groupedStudents[student.class] = [];
-    }
-
-    groupedStudents[student.class].push(student.name);
-
+for (const student of school) {
+    student.passed = student.fee_pays > 2500;
 }
 
-console.log(groupedStudents);
+console.log(school);
