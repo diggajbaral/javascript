d8d5afe0ff8325b3e5c2f79737ab77c2e548b7ba
diggajbaral <volaribrl@gmail.com>
2025-10-20 18:00:41 +0530

    create new obj using push

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ const School = [
     { id: 2, name: "Anjali Sharma", class: 10, fee_pays: 2700 },
     { id: 3, name: "Ramesh Thapa", class: 8, fee_pays: 2300 },
     { id: 4, name: "Sita Rai", class: 9, fee_pays: 2600 },
-    { id: 5, name: "Kiran Gurung", class: 11, fee_pays: 2800 }
+    { id: 5, name: "Kiran Gurung", class: 11, fee_pays: 2800 },
+    { id: 6, name: "Krishna Chaudary", fee_pays: 8600 }
 ];
 
-let sum = 0;
-for (const student of School) {
-    sum += student.fee_pays;
-}
-
-const average = sum / School.length;
-
-console.log('Average fee payment', average);
+let groupedStudents = {};
 
 for (const student of School) {
-    if (student.fee_pays > average) {
-        console.log(student.name);
+
+    if (!groupedStudents[student.class]) {
+        groupedStudents[student.class] = [];
     }
+
+    groupedStudents[student.class].push(student.name);
+
 }
+
+console.log(groupedStudents);
