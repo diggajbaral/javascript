4146e6c24e94db0fe9dc525d7dc287fc37d73a52
diggajbaral <volaribrl@gmail.com>
2025-10-27 00:12:35 +0530

    reverse the string

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-const apiResponse = {
-    users: [
-        { id: 1, name: "Alice", age: 28 },
-        { id: 2, name: "Bob", age: 34 },
-        { id: 3, name: "Charlie", age: 22 },
-        { id: 4, name: "Diana", age: 31 },
-        { id: 5, name: "Eve", age: 28 },
-        { id: 6, name: "Frank", age: 22 }
-    ]
-};
-
-let totalAge = {};
-
-for (user of apiResponse.users) {
-
-    const age = user.age;
-
-    if (totalAge[age]) {
-        totalAge[age]++;
-    } else {
-        totalAge[age] = 1;
-    }
-
+function reverseWord(str) {
+    return str.trim().split(/\s+/).reverse().join(' ')
 }
 
-console.log(totalAge);
+console.log(reverseWord("I love JavaScript"));
