d4be118eaa68d22010122f16ee8b3e92322ccf0a
diggajbaral <volaribrl@gmail.com>
2025-11-14 11:17:00 +0530

    primitive & non-primitive datatype

---
+++ ChaiCode/01_basics/datatypeSummary.js
@@ -0,0 +1,7 @@
+// - - - Primitive Datatype - - - //
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// - - - Reference (Non-Primitive) - - - //
+// Array, Object, Function
+
