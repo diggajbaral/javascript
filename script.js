2fd4d87093a2756a45b400445973c996ad405f00
diggajbaral <volaribrl@gmail.com>
2025-11-15 18:26:44 +0530

    propertise of date and time

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// JavaScript
+const x = new Date();
+console.log(x);
+
+// - - - Propertise of Date & Time - - - //
+
+x.setFullYear(2030);
+x.setMonth(0);
+x.setDate(1);
+x.setHours(12);
+x.setMinutes(10);
+x.setSeconds(59);
+
+// - - - // - - - // - - - //
+
+console.log(x.getFullYear());
+console.log(x.getMonth());
+console.log(x.getDate());
+console.log(x.getDay());
+console.log(x.getHours());
+console.log(x.getMinutes());
+console.log(x.getSeconds());
+console.log(x.getMilliseconds());
+console.log(x.getTime());
+console.log(x.getTimezoneOffset());
