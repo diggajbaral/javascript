b196dd656e8702b653b1ffc6141220fb8eb317fc
diggajbaral <volaribrl@gmail.com>
2025-11-15 18:37:26 +0530

    date and time with proper commit to understant

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const x = new Date();
-console.log(x);
+console.log(x); // 2025-11-15T13:04:49.524Z
 
 // - - - Propertise of Date & Time - - - //
 
@@ -10,16 +10,16 @@ x.setHours(12);
 x.setMinutes(10);
 x.setSeconds(59);
 
-console.log(x.getFullYear());
-console.log(x.getMonth());
-console.log(x.getDate());
-console.log(x.getDay());
-console.log(x.getHours());
-console.log(x.getMinutes());
-console.log(x.getSeconds());
-console.log(x.getMilliseconds());
-console.log(x.getTime());
-console.log(x.getTimezoneOffset());
+console.log(x.getFullYear()); // 2023
+console.log(x.getMonth()); // 0
+console.log(x.getDate()); // 1
+console.log(x.getDay()); // 2
+console.log(x.getHours()); // 12
+console.log(x.getMinutes()); // 10
+console.log(x.getSeconds()); // 59
+console.log(x.getMilliseconds()); // 542
+console.log(x.getTime()); // 1893480059524
+console.log(x.getTimezoneOffset()); // -330
 
 
 
@@ -27,9 +27,9 @@ console.log(x.getTimezoneOffset());
 
 const createdDate = new Date();
 
-console.log(createdDate.toString());
-console.log(createdDate.toDateString());
-console.log(createdDate.toTimeString());
-console.log(createdDate.toISOString());
-console.log(createdDate.toLocaleString());
-console.log(createdDate.toLocaleDateString());
+console.log(createdDate.toString()); // Sat Nov 15 2025 18:34:49 GMT+0530 (India Standard Time)
+console.log(createdDate.toDateString()); // Sat Nov 15 2025
+console.log(createdDate.toTimeString()); // 18:34:49 GMT+0530 (India Standard Time)
+console.log(createdDate.toISOString()); // 2025-11-15T13:04:49.538Z
+console.log(createdDate.toLocaleString()); // 11/15/2025, 6:34:49 PM
+console.log(createdDate.toLocaleDateString()); // 11/15/2025
