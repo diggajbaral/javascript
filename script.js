c66a1a58039c88e61c79e30238dba764c28bfe5c
diggajbaral <volaribrl@gmail.com>
2025-11-16 10:51:58 +0530

    triangle pattern using for loop

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-for (let i = 0; i <= 5; i++) {
-    console.log(i);
+let x = 5;
+
+for (let i = 1; i <= x; i++) {
+
+    let spaces = " ".repeat(x - i);
+    let stars = "*".repeat(2 * i - 1);
+
+    console.log(spaces + stars);
+
 }
