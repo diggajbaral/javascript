ca3d98bb90f6dd98413e198bba9786c0a22e773a
diggajbaral <volaribrl@gmail.com>
2025-11-16 19:20:04 +0530

    count total number of evens from an array

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let sum = 0;
+let nums = [3, 8, 12, 5, 7, 20, 1];
+let count = 0;
 
-for (let i = 1; i <= 100; i++) {
-    sum = sum + i;
+for (let num of nums) {
+    if (num % 2 == 0) {
+        count = count + 1
+    }
 }
 
-console.log(sum);
+console.log(count);
