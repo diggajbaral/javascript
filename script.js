54d76efa002caf0df3197a000d35f29c4321fa88
diggajbaral <volaribrl@gmail.com>
2025-11-17 09:35:24 +0530

    reverse an array without using Set

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let items = ["a", "b", "c", "d"];
-let reversed = [];
+let arr = [1, 2, 2, 3, 4, 4, 5];
+let result = [];
 
-for (let i = items.length - 1; i >= 0; i--) {
-    reversed.push(items[i]);
+for (let item of arr) {
+    if (!result.includes(item)){
+        result.push(item);
+    }
 }
 
-console.log(reversed);
+console.log(result);
+
+// 5th line - "If result does NOT contain the current item, then proceed inside the if block"
