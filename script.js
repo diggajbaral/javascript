c508025c7dfaf540c599725428d55699e3a57ed8
diggajbaral <volaribrl@gmail.com>
2025-11-18 16:06:11 +0530

    Check if an element exists in an array

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 const array = [3, 7, 2, 9, 5, 4];
-let reverse = [];
-
-for (let i = array.length - 1; i >= 0; i--) {
-    reverse.push(array[i])
-}
-
-console.log(reverse);
+console.log(array.includes(7));
+console.log(array.includes(70));
