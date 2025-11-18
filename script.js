46442fd44b8135912b6527f0db1e27f71eb9827b
diggajbaral <volaribrl@gmail.com>
2025-11-18 15:48:45 +0530

    Separating even and odd numbers in an array

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const array = [3, 7, 2, 9, 5];
-let sum = 0;
+const array = [3, 7, 2, 9, 5, 4];
+let evenCount = [];
+let oddCount = [];
 
 for (let arr of array) {
-    sum += arr;
+    if (arr % 2 === 0) {
+        evenCount.push(arr);
+    } else {
+        oddCount.push(arr);
+    }
 }
 
-console.log(sum);
+console.log(evenCount);
+console.log(oddCount);
