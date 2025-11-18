3012c6e640f96c3fef29fd7f328e9151e8c93468
diggajbaral <volaribrl@gmail.com>
2025-11-18 15:39:01 +0530

    Finding largest number in an array using ...XXXXX

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-let arr = [1, 2, 2, 3, 4, 4, 5];
-let result = [];
-
-for (let item of arr) {
-    if (!result.includes(item)){
-        result.push(item);
-    }
-}
-
-console.log(result);
-
-// 5th line - "If result does NOT contain the current item, then proceed inside the if block"
+const array = [3, 7, 2, 9, 5];
+console.log(Math.max(...array));
