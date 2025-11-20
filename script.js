735d9bc1853829859ee211730744dad027de4ccc
diggajbaral <volaribrl@gmail.com>
2025-11-20 21:53:19 +0530

    Adding in array with dual condtional

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const companyCEO = ['elon musk', 'sundar pachai', 'andy jassy', 'ritesh agarwal']
-let blockCEO = []
+const nums = [3, 10, 6, 15, 2, 20];
+let newNums = [];
 
-for (let ceo of companyCEO) {
-    blockCEO.push(ceo.toUpperCase());
+for (let num of nums) {
+    if (num >= 5 && num % 2 == 0) {
+        newNums.push(num);
+    }
 }
 
-console.log(blockCEO);
-
+console.log(newNums);
