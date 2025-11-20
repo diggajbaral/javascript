ee85f876761807736a8e2a7deae365304c314d12
diggajbaral <volaribrl@gmail.com>
2025-11-20 22:00:56 +0530

    Returns a new array containing all numbers greater than 5, but each multiplied by 2

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const nums = [3, 10, 6, 15, 2, 20];
-let newNums = [];
 
-for (let num of nums) {
-    if (num >= 5 && num % 2 == 0) {
-        newNums.push(num);
-    }
-}
+const newNums = nums
+    .filter(num => num > 5)
+    .map(num => num * 2);
 
 console.log(newNums);
