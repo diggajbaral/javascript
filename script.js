c0a37b764c22e0682b26f2d5a1a21aa14d0dde51
diggajbaral <volaribrl@gmail.com>
2025-11-21 20:59:59 +0530

    Return undices of two numbers that add up to target

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const arr = ["a", "b", "a", "c", "b", "a"];
-const freq = {};
-
-for (let item of arr) {
-    if (freq[item]) {
-        freq[item] += 1;
-    } else {
-        freq[item] = 1;
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
     }
+    return [];
 }
 
-console.log(freq);
+console.log(twoSum([2, 7, 11, 15], 9));
