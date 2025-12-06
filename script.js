e880059bc9b1e6364e6f043640d47a598fdb1c52
diggajbaral <volaribrl@gmail.com>
2025-12-06 21:20:45 +0530

    Reversing an array without inbuilt function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const nums = [10, 25, 31, 34, 14, 56];
+const array = [1, 3, 5, 7, 9, 2, 4, 6, 8];
+const reversed = [];
 
-const sorted = nums.sort((a, b) => b - a);
-const secondLargest = sorted[1];
+for (let i = array.length - 1; i >= 0; i--) {
+    reversed.push(array[i]);
+}
 
-console.log(secondLargest);
+console.log(reversed);
