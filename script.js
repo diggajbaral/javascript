f368313c46fcb28dcc2964d65d5d2eed654a3dc5
diggajbaral <volaribrl@gmail.com>
2025-12-06 20:55:32 +0530

    Finding max from an array

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-function isAnagram(a, b) {
-
-  if (a.length !== b.length) return false;
-
-  let one = a.split("").sort().join("");
-  let two = b.split("").sort().join("");
-
-  console.log(one, two);
-
-  return one === two;
-
-}
-
-console.log(isAnagram("listen", "silent"));
+const array = [1, 2, 3, 4, 5, 9, 999, 888, 777]
+console.log(Math.max(...array));
