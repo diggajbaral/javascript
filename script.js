f2c21feef21f50e13ccdede65d5e0b0d5aeb0703
diggajbaral <volaribrl@gmail.com>
2026-01-28 22:41:31 +0530

    Count how many times a given element appears in an array.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const givenNumber = 5;
-let factorial = 1;
+let arr = [3, 7, 2, 1, 9, 5, 5, 1, 3, 2, 6, 7];
+const givenElement = 7;
+let count = 0;
 
-for (let i = 1; i <= givenNumber; i++) {
-    factorial *= i;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === givenElement) {
+        count++;
+    }
 }
 
-console.log(factorial);
+console.log(count);
