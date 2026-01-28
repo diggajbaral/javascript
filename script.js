5ccf0b06800dc0d4a6d1b9bdd93f0c5fe582427a
diggajbaral <volaribrl@gmail.com>
2026-01-28 21:28:01 +0530

    Calculate sum of X natural digits

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let num = 9801309237;
-let sumDigits = 0;
+const natural = 5;
+let sum = 0;
 
-for (let temp = num; temp > 0; temp = Math.floor(temp / 10)) {
-    sumDigits += temp % 10;
+for (let i = 1; i <= natural; i++){
+    sum += i;
 }
 
-console.log(sumDigits);
+console.log(sum);
