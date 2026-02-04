e2564f039a21aa28ac3c94efeb75a61bdb2cdb64
diggajbaral <volaribrl@gmail.com>
2026-02-04 09:57:46 +0530

    Using forEach print even only

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const rst = numbers.map(num => {
-    if (num % 2 === 0) {
-        return "even";
-    } else {
-        return "odd";
-    }
-});
+numbers.forEach(num => {
 
-console.log(rst);
+    if (num % 2 === 0) {
+        console.log(`${num} is even`);
+    }
+
+})
