8c7c9aa6a5fabb14f220906f85bb5fb243395d52
diggajbaral <volaribrl@gmail.com>
2026-02-22 22:49:14 +0530

    Implement nested array flattening using loops

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let n = 10;
-let a = 0, b = 1;
+let arr = [[1, 2], [3, 4], [5, 6]];
+let flat = [];
 
-for (let i = 1; i <= n; i++) {
-  console.log(a);
-  let next = a + b;
-  a = b;
-  b = next;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    flat.push(arr[i][j]);
+  }
 }
+
+console.log(flat);
